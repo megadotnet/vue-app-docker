458a7ef8ffdb73cefb1dd8283d5f7e75451c7916
megadotnet <wintersunair@gmail.com>
2025-02-09 15:28:46 +0800

    build(deps): 添加测试工具并更新依赖
    - 添加 @vue/test-utils 作为开发依赖
    - 更新 package-lock.json 中的多个依赖包版本
    - 移除重复的 compression-webpack-plugin 和 dotenv 依赖

---
+++ src/components/ClickCounter.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <button @click="increment">Click me</button>
+      <p>You have clicked the button {{ count }} times.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ClickCounter',
+    data() {
+      return {
+        count: 0
+      };
+    },
+    methods: {
+      increment() {
+        this.count += 1;
+      }
+    }
+  };
+  </script>
+  
